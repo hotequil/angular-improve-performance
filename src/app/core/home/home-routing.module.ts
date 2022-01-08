a35cd5b89017c2d61a0546f81087386b29aff9ab
hotequil <hotequil@protonmail.com>
2022-01-08 15:11:46 -0300

    feat: init home module

--- src/app/core/home/home-routing.module.ts
+++ src/app/core/home/home-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeModule } from "./core/home/home.module";
+import { HomeComponent } from "./home.component";
 
 const routes: Routes = [
     {
         path: '',
-        loadChildren: () => HomeModule
+        component: HomeComponent
     }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule{}
