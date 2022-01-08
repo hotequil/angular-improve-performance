6fd8b10a540ba7aa4d3757c692cbe2fd0ceea290
hotequil <hotequil@protonmail.com>
2022-01-08 20:50:46 -0300

    feat: adjust style and create fn calculateSumOfCustomerBalances.

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Customer } from "../../shared/types/customer";
+import { CustomersService } from "../../shared/services/customers.service";
+import { counter } from "../../shared/decorators/counter";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-    constructor(){}
+export class HomeComponent implements OnInit, OnDestroy{
+    customers: Customer[] = [];
 
-    ngOnInit(): void{}
+    private intervalId!: number;
+
+    constructor(private customersService: CustomersService, private changeDetectorRef: ChangeDetectorRef){}
+
+    async ngOnInit(): Promise<void>{
+        this.customers = await this.customersService.get();
+        this.intervalId = setInterval(() => this.changeDetectorRef.detectChanges(), 1000);
+    }
+
+    @counter("Function")
+    calculateSumOfCustomerBalances(): number{
+        return this.customers.reduce((accumulator, item) => accumulator + item.balance, 0);
+    }
+
+    ngOnDestroy(): void{
+        clearInterval(this.intervalId);
+    }
 }
--- src/app/core/home/home.module.ts
+++ src/app/core/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from "./home-routing.module";
+import { CalculateSumOfCustomerBalancesModule } from "../../shared/pipes/calculate-sum-of-customer-balances/calculate-sum-of-customer-balances.module";
 
 @NgModule({
     declarations: [
@@ -10,7 +11,8 @@ import { HomeRoutingModule } from "./home-routing.module";
     ],
     imports: [
         CommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        CalculateSumOfCustomerBalancesModule
     ]
 })
 export class HomeModule{}
